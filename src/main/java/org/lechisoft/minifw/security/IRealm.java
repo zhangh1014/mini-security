e7c58b18d4093e571bc0877f6745c6efc96eeb31
hao <hao@DESKTOP-2NCL7JG>
2018-01-18 19:59:36 +0800

    lala

--- src/main/java/org/lechisoft/minifw/security/IRealm.java
+++
@@ -1,9 +0,0 @@
-package org.lechisoft.minifw.security;
-
-import org.lechisoft.minifw.security.model.UserModel;
-
-public interface IXmlRealm {
-    public void load();
-    
-    public void addUser(UserModel user);
-}
