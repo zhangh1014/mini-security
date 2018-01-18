15eb93083808b44ba979be9ddf32a975e4229ba2
Hao <Hao@192.168.56.1>
2018-01-19 01:14:22 +0800

    lalala

--- src/main/java/org/lechisoft/minifw/security/common/ConstValue.java
+++
@@ -1,9 +0,0 @@
-package org.lechisoft.minifw.security.common;
-
-public class ConstValue {
-    public final static String DEFAULT_LOGGER = "syslogger";
-    public final static String AUTHENTICATION_INFO = "authentication";
-    public final static String AUTHORIZATION_INFO = "authorization";
-    public final static String HASH_ALGORITHM_NAME ="MD5";
-    public final static String SESSION_LOGIN_OBJECT_KEY = "loginObject";
-}
